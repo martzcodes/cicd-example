e997e73b2e7006e5e7c2f25b932ef6403dd09751
martzcodes <978362+martzcodes@users.noreply.github.com>
2023-11-05 21:57:19 -0500

    add api and lambda

--- lib/cicd-example-stack.ts
+++ lib/cicd-example-stack.ts
@@ -1,16 +1,31 @@
 import * as cdk from 'aws-cdk-lib';
+import { RestApi, EndpointType, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { Construct } from 'constructs';
+import { join } from 'path';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CicdExampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const fn = new NodejsFunction(this, `DummyFn`, {
+      entry: join(__dirname, `lambda/dummy.ts`),
+      runtime: Runtime.NODEJS_18_X,
+      timeout: cdk.Duration.minutes(15),
+      logRetention: RetentionDays.TWO_WEEKS,
+      memorySize: 1024,
+      retryAttempts: 0,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CicdExampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const api = new RestApi(this, `ExampleApi`, {
+      description: `Example API`,
+      endpointConfiguration: {
+        types: [EndpointType.REGIONAL],
+      },
+    });
+    api.root.addResource(`dummy`).addMethod(`GET`, new LambdaIntegration(fn));
   }
 }
